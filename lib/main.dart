3c3ca1ee3eed3db1470ea5957d13561668e48e71
jruschill1 <jruschil@uncc.edu>
2022-05-19 15:51:52 -0400

    idk

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:reminder_app/Screens/add_note.dart';
-
-String url = "gradient.jpg";
-void main() {
-  runApp(const MaterialApp(home: AddNote()));
-}
