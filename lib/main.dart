f14ae17460118f8f87c699942a885a7b1dc50930
Jarod <jruschil@uncc.edu>
2022-05-19 15:23:18 -0400

    Jarod's bullshit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:reminder_app/Screens/add_note.dart';
+
+String url = "gradient.jpg";
+void main() {
+  runApp(const MaterialApp(home: AddNote()));
+}
