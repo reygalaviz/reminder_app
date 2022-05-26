c1172c00fcc28f33a02fcd351fab5a9c39ab3d86
reygalaviz <reygalaviz26@yahoo.com>
2022-05-25 23:34:50 -0400

    settings tab

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,8 +6,10 @@ import 'package:reminder_app/models/color_data.dart';
 import 'package:reminder_app/models/note_data.dart';
 import 'package:reminder_app/models/datetime_data.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:reminder_app/screens/add_note.dart';
 import 'package:reminder_app/screens/all_notes.dart';
 import 'package:reminder_app/screens/calendar.dart';
+import 'package:reminder_app/controllers/Notifications.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,6 +19,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String body = "";
+
   int _selectedIndex = 0;
   PageController pageController = PageController();
 
@@ -30,8 +34,17 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Reminders'),
+        actions: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                  onPressed: () => showSettingsModal(),
+                  icon: Icon(Icons.settings)))
+        ],
+      ),
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(title: Text('Reminders')),
       body: PageView(
         controller: pageController,
         physics: NeverScrollableScrollPhysics(),
@@ -39,6 +52,16 @@ class _HomeState extends State<Home> {
           AllNotes(),
           Calendar(),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.grey[900],
+        onPressed: () {
+          return showTextboxKeyboard();
+        },
+        child: const Icon(Icons.add),
       ),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
@@ -66,11 +89,61 @@ class _HomeState extends State<Home> {
             ],
             currentIndex: _selectedIndex,
             selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.grey,
+            unselectedItemColor: Color.fromARGB(255, 122, 122, 122),
             onTap: onTapped,
           ),
         ),
       ),
     );
+  }
+
+  void showSettingsModal() {
+    showModalBottomSheet(
+        context: context,
+        enableDrag: true,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20.0))),
+        builder: (context) {
+          return Container(height: 850, child: Column());
+        });
+  }
+
+  //keyboard textfield attachment
+  void showTextboxKeyboard() {
+    showModalBottomSheet(
+        enableDrag: false,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20.0))),
+        context: context,
+        builder: (context) {
+          return SingleChildScrollView(
+            reverse: true,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0),
+                child: Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        onChanged: (value) => body = value,
+                        autofocus: true,
+                      ),
+                      const SizedBox(height: 10),
+                      TextButton(
+                        onPressed: () {
+                          NotificationService().displayNotification(body: body);
+                        },
+                        child: const Text("Submit"),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
