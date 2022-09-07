3425a389c853f9a5017d7a001c7e9d1fa4acadf8
Kimberly Hawks <kimberlyhawks@Kimberlys-MBP.attlocal.net>
2022-09-07 00:04:22 -0400

    updated support ui

--- lib/screens/settings-support.dart
+++ lib/screens/settings-support.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:reminder_app/screens/settings.dart';
+import 'package:reminder_app/themes/theme_model.dart';
 
 class SettingsSupport extends StatefulWidget {
   const SettingsSupport({Key? key}) : super(key: key);
@@ -11,117 +12,246 @@ class SettingsSupport extends StatefulWidget {
 }
 
 class _SettingsSupportState extends State<SettingsSupport> {
+  TextEditingController description = TextEditingController();
+  TextEditingController details = TextEditingController();
+  TextEditingController email = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: LayoutBuilder(
-          builder: (context, constraints) => Container(
-                color: Theme.of(context).backgroundColor,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            'Hey, how can we help?',
-                            style: TextStyle(fontSize: 20),
+    _showSnackBar(BuildContext context) {
+      final snackbar = SnackBar(
+        padding: EdgeInsets.only(top: 15),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20.0))),
+        duration: Duration(seconds: 3),
+        content: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Form Submitted',
+              style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.green[200],
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+    }
+
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Material(
+        child: LayoutBuilder(
+            builder: (context, constraints) => Container(
+                  color: Theme.of(context).backgroundColor,
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            color: Colors.blue[100],
+                            height: constraints.maxHeight * .15,
+                            width: double.infinity,
                           ),
-                        ),
-                        Form(
-                            key: _formKey,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Padding(
+                          Container(
+                            padding: EdgeInsets.only(
+                                top: constraints.maxHeight * .05),
+                            child: Center(
+                                child: Text(
+                              'Hey, how can we help?',
+                              style: TextStyle(
+                                  color: Colors.blue[900],
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Poppins'),
+                            )),
+                          )
+                        ],
+                      ),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(top: 15),
+                                child: Padding(
                                   padding: EdgeInsets.all(8.0),
                                   child: Text(
                                     'Describe the problem you are having',
-                                    style: TextStyle(fontSize: 15),
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
                                   ),
                                 ),
-                                Padding(
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).backgroundColor,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color:
-                                            Theme.of(context).backgroundColor,
-                                        borderRadius:
-                                            BorderRadius.circular(10.0)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: TextFormField(
-                                        decoration: const InputDecoration(
-                                          border: InputBorder.none,
-                                        ),
-                                      ),
-                                    ),
+                                  child: TextFormField(
+                                    controller: description,
+                                    validator: (val) {
+                                      if (val!.isEmpty ||
+                                          !RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$")
+                                              .hasMatch(val)) {
+                                        return 'Please enter a valid input';
+                                      } else {
+                                        return null;
+                                      }
+                                    },
+                                    cursorColor: Theme.of(context).primaryColor,
+                                    maxLines: 2,
+                                    decoration: InputDecoration(
+                                        errorBorder: const OutlineInputBorder(
+                                            borderSide:
+                                                BorderSide(color: Colors.red)),
+                                        focusedErrorBorder:
+                                            const OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.red)),
+                                        contentPadding:
+                                            const EdgeInsets.all(6.0),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .primaryColor)),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .primaryColor))),
                                   ),
                                 ),
-                                const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Give us more details',
-                                    style: TextStyle(fontSize: 15),
-                                  ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Give us more details',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                Padding(
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).backgroundColor,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color:
-                                            Theme.of(context).backgroundColor,
-                                        borderRadius:
-                                            BorderRadius.circular(10.0)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: TextFormField(
-                                        decoration: const InputDecoration(
-                                          border: InputBorder.none,
-                                        ),
-                                      ),
-                                    ),
+                                  child: TextFormField(
+                                    controller: details,
+                                    validator: (val) {
+                                      if (val!.isEmpty ||
+                                          !RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$")
+                                              .hasMatch(val)) {
+                                        return 'Please enter a valid input';
+                                      } else {
+                                        return null;
+                                      }
+                                    },
+                                    cursorColor: Theme.of(context).primaryColor,
+                                    maxLines: 5,
+                                    decoration: InputDecoration(
+                                        errorBorder: const OutlineInputBorder(
+                                            borderSide:
+                                                BorderSide(color: Colors.red)),
+                                        focusedErrorBorder:
+                                            const OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.red)),
+                                        contentPadding: EdgeInsets.all(6.0),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .primaryColor)),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .primaryColor))),
                                   ),
                                 ),
-                                const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Enter email',
-                                    style: TextStyle(fontSize: 15),
-                                  ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Enter Email Address',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                                Padding(
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).backgroundColor,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        color:
-                                            Theme.of(context).backgroundColor,
-                                        borderRadius:
-                                            BorderRadius.circular(10.0)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: TextFormField(
-                                        decoration: const InputDecoration(
-                                          border: InputBorder.none,
-                                        ),
-                                      ),
-                                    ),
+                                  child: TextFormField(
+                                    controller: email,
+                                    validator: (val) {
+                                      if (val!.isEmpty ||
+                                          !RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                              .hasMatch(val)) {
+                                        return 'Please enter a valid email address';
+                                      } else {
+                                        return null;
+                                      }
+                                    },
+                                    cursorColor: Theme.of(context).primaryColor,
+                                    maxLines: 1,
+                                    decoration: InputDecoration(
+                                        errorBorder: const OutlineInputBorder(
+                                            borderSide:
+                                                BorderSide(color: Colors.red)),
+                                        focusedErrorBorder:
+                                            const OutlineInputBorder(
+                                                borderSide: BorderSide(
+                                                    color: Colors.red)),
+                                        contentPadding:
+                                            const EdgeInsets.all(6.0),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .primaryColor)),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Theme.of(context)
+                                                    .primaryColor))),
                                   ),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ElevatedButton(
-                                      onPressed: () {}, child: Text('Submit')),
-                                )
-                              ],
-                            )),
-                      ],
-                    ),
-                  ],
-                ),
-              )),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.blue[100])),
+                                    onPressed: () {
+                                      if (_formKey.currentState!.validate()) {
+                                        description.clear();
+                                        details.clear();
+                                        email.clear();
+                                        return _showSnackBar(context);
+                                      }
+                                    },
+                                    child: Text('Submit',
+                                        style: TextStyle(
+                                            color: Theme.of(context)
+                                                .primaryColor))),
+                              ),
+                            ],
+                          )),
+                    ],
+                  ),
+                )),
+      ),
     );
   }
 }
